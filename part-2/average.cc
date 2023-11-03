a36d88697b554e3b7e61427b8e1cf0014ddd09b7
Julian Kasden <juliankasden@csu.fullerton.edu>
2023-11-02 17:12:37 -0700

    I added my partners name to the header

--- part-2/average.cc
+++ part-2/average.cc
@@ -10,7 +10,7 @@
 int main(int argc, char* argv[]) {
   std::vector<std::string> arguments{argv, argv + argc};
 
-  // XTODO: Validate that there is at least one command line argument.
+  // XTOD: Validate that there is at least one command line argument.
   // If not, print an error message and return a non-zero value.
   if (arguments.size() <= 1) {
     std::cout << "error: you must supply at least one number";
